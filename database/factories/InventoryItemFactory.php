78ad5d358c1708cea4dce5ea64e3a1b465d61b65
sohel ahmed <sohel@gmail.com>
2024-04-02 23:05:57 +0600

    DB created

---
+++ database/factories/InventoryItemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InventoryItem>
+ */
+class InventoryItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'inventory_id' => mt_rand(1, 50),
+            'image' => 'https://picsum.photos/id/20/575/350',
+            'quantity' => mt_rand(1, 100),
+            'description' => fake()->paragraph(),
+        ];
+    }
+}
---
+++ database/seeders/InventoryItemSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\InventoryItem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class InventoryItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        InventoryItem::factory(500)->create();
+    }
+}
